a3135ff59070beb6ef25a34aa9e9d8dc5345dad4
qixuehui <53709034+qixuehui@users.noreply.github.com>
2020-06-08 18:52:03 +0800

    Add files via upload

---
+++ bibi/index/index.js
@@ -0,0 +1,106 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 被点击的首页导航的菜单的索引
+    currentIndexNav:0,
+    navList:[],
+    // 轮播图
+    SwiperList:[]
+  },
+  // 点击导航按钮
+  activeNav(e){
+    this.setData({
+      currentIndexNav:e.target.dataset.index
+    })
+  },
+  // 获取导航数据的函数
+  getNavList(){
+    let that=this;
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/navList',
+      success(res){
+        // console.log(res);
+        if(res.data.code==0){
+          that.setData({
+            navList:res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+  // 获取轮播图导航的函数
+  SwiperLis(){
+    let that=this;
+    wx.request({
+      url: 'http://mock-api.com/mnEe4VnJ.mock/swiperList',
+      success(res){
+         console.log(res);
+          that.setData({
+            SwiperList:res.data.data.swiperList
+          })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 获取首页
+    this.getNavList();
+    //获取轮播图
+    this.SwiperLis();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
